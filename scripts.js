1f0d8b52dd238a05553c1ecdd6a0b6b685c5f963
Ian Civgin <imciv2@gmail.com>
2015-07-02 22:36:22 -0700

    Added modal, button, Post constructor, Post prototype methods, post template, and submit and delete basic skeletons.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,56 @@
 $(function() {
 
+var $yourName = $('#your-name');
+var $postTitle = $('#post-title');
+var $content = $('#content');
+var $newPostForm = $('new-post-form');
 
+// Post constructor and prototype functions
+function Post (owner, title, content) {
+	var date = new Date()
+	this.owner = owner;
+	this.title = title;
+	this.content = content;
+	this.highFiveCount = 0;
+	this.date = date.toDateString();
+	this.index = Post.count;
+}
+
+Post.count = 0;
+
+Post.prototype.save = function() {
+
+}
+
+Post.prototype.render = function() {
+
+}
+
+Post.prototype.delete = function() {
+
+}
+
+Post.prototype.highFive = function() {
+
+}
+
+// Modal publish button
+$newPostForm.on('submit', function() {
+
+});
+
+// Post delete button
+$newPostForm.on('submit', '.delete', function() {
+
+
+});
+
+var x = new Post('Ian Civgin', 'Test', 'lasjcasiahoiesfasdf,aelfia');
+
+console.log(x);
+
+$('#new-post').on('shown.bs.modal', function () {
+  $('#your-name').focus()
+})
 
 })
